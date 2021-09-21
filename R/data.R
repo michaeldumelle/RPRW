8325e25bc959852750680fa060f495de9b1ed90a
Michael Dumelle <mjdumelle@gmail.com>
2021-09-20 18:17:09 -0700

    fixed typo in data documentation.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' River data
 #'
-#' An data frame containing length and discharge of several river
+#' An data frame containing length and discharge of several rivers
 #'
 #' @format A data frame with 2 rows (\code{length} and \code{discharge}) and 8 variables:
 #' \describe{
